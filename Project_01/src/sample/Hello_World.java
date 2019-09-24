d7dba9f011a56e2a639229a38cdc13fdc5896343
ManojGitHub6 <manojeligeti666@gmail.com>
2019-09-24 17:32:01 +0530

    First Commit

---
+++ Project_01/src/sample/Hello_World.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Hello_World {
+	
+	public static void main(String[] args) {
+		
+		int a=2;
+		int b=4;
+		if(b>a) {
+			System.out.println("B is greater");
+		}
+		
+	}
+
+}
